d2d0933fa1c1bcc71770646aecb6d9ff22c66c16
martinflower <wisemonk75@gmail.com>
2018-01-18 15:32:44 +0000

    new home design

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title> @yield('title') </title>
     <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-    <link rel="stylesheet" href="{!! asset('css/superhero.bootstrap.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}">
     <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
     <script src="{!! asset('js/jquery-3.2.1.min.js') !!}"></script>
     <!-- Include roboto.css to use the Roboto web font, material.css to include \
@@ -11,7 +11,14 @@
     <link rel="stylesheet" type="text/css" href="{!! asset('css/roboto.min.css" rel="stylesheet') !!}">
     <link rel="stylesheet" type="text/css" href="{!! asset('css/material.min.css" rel="stylesheet') !!}">
     <link rel="stylesheet" type="text/css" href="{!! asset('/css/ripples.min.css" rel="stylesheet') !!}">
-
+    <script src="/js/ripples.min.js"></script>
+    <script src="/js/material.min.js"></script>
+    <script>
+        $(document).ready(function() {
+// This command is used to initialize some elements and make them work properly
+            $.material.init();
+        });
+    </script>
 </head>
 <body>
 @include('shared.navbar')

1278257da58d71906baaa8fb6e8baec099c8260c
martinflower <wisemonk75@gmail.com>
2018-01-22 08:54:59 +0000

    new home design v1.0

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,27 +1,30 @@
 <html>
 
+<html lang="Fr">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title> @yield('title') </title>
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >
-    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}">
-    <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
-    <script src="{!! asset('js/jquery-3.2.1.min.js') !!}"></script>
+    <!--<link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" >-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/material.min.css') !!}">
     <!-- Include roboto.css to use the Roboto web font, material.css to include \
     the theme and ripples.css to style the ripple effect -->
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/roboto.min.css" rel="stylesheet') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('css/material.min.css" rel="stylesheet') !!}">
-    <link rel="stylesheet" type="text/css" href="{!! asset('/css/ripples.min.css" rel="stylesheet') !!}">
-    <script src="/js/ripples.min.js"></script>
-    <script src="/js/material.min.js"></script>
-    <script>
-        $(document).ready(function() {
-// This command is used to initialize some elements and make them work properly
-            $.material.init();
-        });
-    </script>
 </head>
 <body>
 @include('shared.navbar')
 @yield('content')
+<script src="{!! asset('js/jquery-3.3.1.min.js') !!}"></script>
+<script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+<!-- <script src="{!! asset('js/material.min.js') !!}"></script>
+<script>
+    $(document).ready(function() {
+// This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script> -->
+
 </body>
 </html>
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -2,8 +2,7 @@
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="c\
-ollapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -18,8 +17,7 @@ ollapse" data-target="#bs-example-navbar-collapse-1">
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" r\
-                       ole="button" aria-expanded="false">Member <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Member <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="/users/register">Register</a></li>
                         <li><a href="/users/login">Login</a></li>
